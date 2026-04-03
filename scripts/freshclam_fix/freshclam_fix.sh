25b4ddffce9b3f16d0a316ba54bd77b9d2ee9929
Ram Nezer <rncv909090@gmail.com>
2026-04-03 23:00:28 +0300

    Delete scripts directory

--- scripts/freshclam_fix/freshclam_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-sudo systemctl stop clamav-freshclam
-sudo freshclam  >freshclam_error.log 2>&1
-sudo systemctl start clamav-freshclam
--- scripts/notify_send.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo -i perl -pi -e 'if($.==6){s/\n/\/.Xauthority"\n/g}if(eof){$.=0}' /etc/systemd/system/notify-send.service*
